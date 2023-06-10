c79d47f94b4e19abee6ec9e5f4599804302dee86
zootedb0t <05kraken@proton.me>
2023-06-10 22:17:24 +0530

    use swww to set wallpaper

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
+# Edit bellow to control the images transition
+export SWWW_TRANSITION_FPS=60
+export SWWW_TRANSITION_STEP=3
+
 wal_dir=/home/stoney/Pictures/walls/
 
 if [ -z "$1" ]; then
-	wall="$(fd . "$wal_dir" -e jpg -e jpeg -e png --type f | shuf -n 1)"
+	wall="$(fd . "$wal_dir" -e jpg -e jpeg -e png -e gif --type f | shuf -n 1)"
 else
 	wall="$1"
 fi
 
-convert "$wall" ~/.local/share/bg.jpg
-wal -nqsic ~/.local/share/bg.jpg
+# Generate colors using pywal
+wal -c
+wal -nqsi "$wall"
 
 # Generate zathura, dunst and rofi colorscheme
 "$HOME"/.local/bin/pywalzathura &
 "$HOME"/.local/bin/dunst_color.sh &
 "$HOME"/.local/bin/pywalrofi &
 
-if pgrep -x "swaybg" >/dev/null; then
-	killall swaybg
-fi
-
-swaybg -i ~/.local/share/bg.jpg -m fill &
+swww img "$wall"
