0cb578231b62156bcb0ef8948ff5d7e87a227261
zootedb0t <05kraken@proton.me>
2024-05-04 13:45:50 +0530

    update config

--- scripts/gtkthemes.sh
+++ scripts/gtkthemes.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
-## Set GTK Themes, Icons, Cursor and Fonts
+# Set GTK Themes, Icons, Cursor and Fonts
 
 THEME='Adwaita'
 ICONS='Papirus-Dark'
-FONT='JetBrains Mono 12'
-CURSOR='Adwaita'
+# FONT='JetBrains Mono 14'
+FONT='Liga SFMono Nerd Font 12'
+CURSOR='Adwaita-Hyprcursor'
 CURSOR_SIZE='24'
 
 SCHEMA='gsettings set org.gnome.desktop.interface'
--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -2,33 +2,35 @@
 
 # Set variable
 wal_dir=/home/stoney/Pictures/walls/
-local_bin_dir=/home/stoney/.local/bin/
-current_wallpaper=/home/stoney/.local/share/bg.jpg
+# local_bin_dir=/home/stoney/.local/bin/
+# current_wallpaper=/home/stoney/.local/share/bg.jpg
 
-# Find wallpaper
+# Select wallpaper
 if [ -z "$1" ]; then
 	wall="$(fd . "$wal_dir" -e jpg -e jpeg -e png -e gif --type f | shuf -n 1)"
 else
 	wall="$1"
 fi
 
-# Generate colors using pywal
-ln -sf "$wall" "$current_wallpaper"
-wal -c
-wal -nqsi "$wall"
+# Generate colors using pywal or matugen
+matugen -t scheme-rainbow image "$wall"
+
+# ln -sf "$wall" "$current_wallpaper"
+# wal -c
+# wal -nqsi "$wall"
 
 # Generate zathura and dunst colorscheme
-"$local_bin_dir/pywalzathura"
-"$local_bin_dir/dunst_color.sh"
+# "$local_bin_dir/pywalzathura"
+# "$local_bin_dir/dunst_color.sh"
 
 # Restart hyprpaper if running
-if pgrep -x "hyprpaper" >/dev/null; then
-	killall hyprpaper
-	hyprpaper &
-fi
+# if pgrep -x "hyprpaper" >/dev/null; then
+# 	killall hyprpaper
+# 	hyprpaper &
+# fi
 
 # Restart waybar if running
-if pgrep -x "waybar" >/dev/null; then
-	killall waybar
-	waybar >/dev/null 2>&1 &
-fi
+# if pgrep -x "waybar" >/dev/null; then
+# 	killall waybar
+# 	waybar >/dev/null 2>&1 &
+# fi
